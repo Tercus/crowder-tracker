e2349f17569082d1d5fd054e852a8af6e2cc91b1
Tercus <Terces86@gmail.com>
2016-04-07 23:36:25 +0200

    enable udp/http for testing might drop WebRTC support
    Maybe I don't even need WebRTC at all.

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var Server = require('bittorrent-tracker').Server
 
 var server = new Server({
-  udp: false, // enable udp server? [default=true]
-  http: false, // enable http server? [default=true]
+  udp: true, // enable udp server? [default=true]
+  http: true, // enable http server? [default=true]
   ws: true, // enable websocket server? [default=true]
   stats: true, // enable web-based statistics? [default=true]
   //filter: function (infoHash, params, cb) {}
 })
 
 // Internal http, udp, and websocket servers exposed as public properties.
-//server.http
-//server.udp
+server.http
+server.udp
 server.ws
 
 server.on('error', function (err) {
@@ -25,8 +25,8 @@ server.on('warning', function (err) {
 
 server.on('listening', function () {
   // fired when all requested servers are listening
-//  console.log('listening on http port:' + server.http.address().port)
-//  console.log('listening on udp port:' + server.udp.address().port)
+  console.log('listening on http port:' + server.http.address().port)
+  console.log('listening on udp port:' + server.udp.address().port)
   console.log('listening on ws port:' + server.ws.address().port)
 })
 
