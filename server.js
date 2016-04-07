a812cab6e0f340a0a048d8a398c4d8ae666e0907
Tercus <Terces86@gmail.com>
2016-04-07 02:43:41 +0200

    Tracker only on websocket

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var Server = require('bittorrent-tracker').Server
 
 var server = new Server({
-  udp: true, // enable udp server? [default=true]
-  http: true, // enable http server? [default=true]
+  udp: false, // enable udp server? [default=true]
+  http: false, // enable http server? [default=true]
   ws: true, // enable websocket server? [default=true]
   stats: true, // enable web-based statistics? [default=true]
   //filter: function (infoHash, params, cb) {}
 })
 
 // Internal http, udp, and websocket servers exposed as public properties.
-server.http
-server.udp
+//server.http
+//server.udp
 server.ws
 
 server.on('error', function (err) {
@@ -25,8 +25,8 @@ server.on('warning', function (err) {
 
 server.on('listening', function () {
   // fired when all requested servers are listening
-  console.log('listening on http port:' + server.http.address().port)
-  console.log('listening on udp port:' + server.udp.address().port)
+//  console.log('listening on http port:' + server.http.address().port)
+//  console.log('listening on udp port:' + server.udp.address().port)
   console.log('listening on ws port:' + server.ws.address().port)
 })
 
@@ -40,10 +40,14 @@ server.listen(8080, 'localhost', (err) => {
 
 server.on('start', function (addr) {
   console.log('got start message from ' + addr)
+  console.log('Array of infoHashes: ' + Object.keys(server.torrents))
 })
 
 server.on('complete', function (addr) {})
-server.on('update', function (addr) {})
+server.on('update', function (addr) {
+	console.log('Update from: ' + addr)
+	console.log('Array of infoHashes: ' + Object.keys(server.torrents))
+})
 server.on('stop', function (addr) {})
 
 // get info hashes for all torrents in the tracker server
